9ca3efa2f0b9c228a5114e6dba4a185591c5b268
Emile <bvcxzq1@gmail.com>
2023-06-28 18:19:10 +0000

    Amend User Model

--- website/models.py
+++ website/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     email = models.CharField(max_length=200, unique=True)
     discord = models.CharField(max_length=200, unique=True)
+    games = models.CharField(max_length=500)
     created_on = models.DateTimeField(auto_now_add=True)
     # Add Time Zone and Times
 
