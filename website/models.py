f93d87c9e2e4538bde0e699076387a78a9efd7c4
Emile <bvcxzq1@gmail.com>
2023-07-16 11:46:15 +0000

    Test User Model in Admin, amend def __str__

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ from cloudinary.models import CloudinaryField
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     email = models.CharField(max_length=200, unique=True)
-    discord = models.CharField(max_length=200, unique=True)
+    discord = models.CharField(max_length=200)
     games = models.CharField(max_length=500)
     created_on = models.DateTimeField(auto_now_add=True)
     # Add Time Zone and Times
@@ -17,7 +17,7 @@ class User(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return self.title
+        return self.username
 
 
 class Event(models.Model):
