9e4d766e7752e868bb38a7e499cc90322bd41532
Emile <bvcxzq1@gmail.com>
2023-07-16 12:11:28 +0000

    Install AllAuth and templates.

--- gamecalendar/settings.py
+++ gamecalendar/settings.py
@@ -42,12 +42,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django_summernote',
     'django.contrib.staticfiles',
     'cloudinary',
     'website',
+    'allauth',
+    'allauth.account',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
